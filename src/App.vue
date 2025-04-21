da484884d7560a4d2f49cb5c7f443c6fab047716
Lior Silman (Calanit IT LTD) <v-liorsilman@microsoft.com>
2025-04-21 15:37:58 +0300

    Initial vue projecy with vite

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<template>
+    <div>
+        <HelloWorld /> 
+
+    </div>
+</template>
+
+<script lang="ts">
+    import Vue from 'vue'
+    import { defineComponent } from 'vue'
+    import HelloWorld from './components/HelloWorld.vue'
+</script>
+
+<style scoped>
+
+</style>
