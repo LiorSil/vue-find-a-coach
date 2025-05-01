343c9ff0d5db18edc681455c5bce580cde874229
Lior Silman <lior.silmansoad@gmail.com>
2025-05-01 20:37:23 +0300

    refactor: update App.vue layout and styling for improved navigation and background
    - Replaced the main div structure with a header containing navigation links for better accessibility.
    - Added background images for light and dark themes, enhancing visual appeal.
    - Improved header styling for a more polished user interface.

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,16 @@
 <template>
-  <div>
-    <NavBar />
-    <router-view v-slot="slotProps">
-      <transition name="route" mode="out-in">
-        <component :is="slotProps.Component"></component>
-      </transition>
-    </router-view>
-  </div>
+  <header>
+    <nav>
+      <h1>Find a Coach</h1>
+      <router-link to="/">All Coaches</router-link> |
+      <router-link to="/requests">Requests</router-link>
+    </nav>
+  </header>
+  <router-view v-slot="slotProps">
+    <transition name="route" mode="out-in">
+      <component :is="slotProps.Component"></component>
+    </transition>
+  </router-view>
 </template>
 
 <script lang="ts">
@@ -24,6 +28,8 @@ export default defineComponent({
 <style>
 @import url("https://fonts.googleapis.com/css2?family=Roboto:wght@400;700&display=swap");
 
+
+
 * {
   box-sizing: border-box;
 }
@@ -34,6 +40,79 @@ html {
 
 body {
   margin: 0;
+  min-height: 100vh;
+  background-image: var(--background-image);
+  background-size: cover;
+  background-position: center;
+  background-attachment: fixed;
+  background-repeat: no-repeat;
+}
+
+:root {
+  --background-image: url('@/assets/light-mode-background.png');
+}
+
+[data-theme="dark"] {
+  --background-image: url('@/assets/dark-mode-background.png');
+}
+
+header {
+  width: 100%;
+  height: 5rem;
+  background-color: #3d008d;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+
+header a {
+  text-decoration: none;
+  color: #f391e3;
+  display: inline-block;
+  padding: 0.75rem 1.5rem;
+  border: 1px solid transparent;
+}
+
+a:active,
+a:hover,
+a.router-link-active {
+  border: 1px solid #f391e3;
+}
+
+h1 {
+  margin: 0;
+}
+
+h1 a {
+  color: white;
+  margin: 0;
+}
+
+h1 a:hover,
+h1 a:active,
+h1 a.router-link-active {
+  border-color: transparent;
+}
+
+header nav {
+  width: 90%;
+  margin: auto;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+}
+
+header ul {
+  list-style: none;
+  margin: 0;
+  padding: 0;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+
+header li {
+  margin: 0 0.5rem;
 }
 
 .route-enter-from {
@@ -58,5 +137,12 @@ body {
 .route-leave-from {
   opacity: 1;
   transform: translateY(0);
+}
+#app {
+  background-image: url('./assets/light-mode-background.png');
+  background-size: cover; /* or `contain`, `repeat` etc. */
+  background-repeat: repeat;
+  background-attachment: fixed;
+  min-height: 100vh;
 }
 </style>
